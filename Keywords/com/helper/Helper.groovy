9c81b5771dccea361fb2b5309eb19d77852659e1
AndikaHarisS <andika.saputra@kpvendor.com>
2024-08-02 16:31:08 +0700

    init

---
+++ Keywords/com/helper/Helper.groovy
@@ -0,0 +1,42 @@
+package com.helper
+
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+
+import com.kms.katalon.core.annotation.Keyword
+import com.kms.katalon.core.checkpoint.Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling
+import com.kms.katalon.core.testcase.TestCase
+import com.kms.katalon.core.testdata.TestData
+import com.kms.katalon.core.testobject.TestObject
+import com.kms.katalon.core.util.KeywordUtil
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+
+import internal.GlobalVariable
+import net.sf.jasperreports.engine.JasperCompileManager
+import net.sf.jasperreports.engine.JasperExportManager
+import net.sf.jasperreports.engine.JasperFillManager
+import net.sf.jasperreports.engine.JasperPrint
+import net.sf.jasperreports.engine.JasperReport
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource
+
+public class Helper {
+
+	public report() {
+		String filePath = "./Data/Invoice.jrxml"
+		JasperReport report = JasperCompileManager.compileReport(filePath)
+		Map<String, Object> parameters = new HashMap<String, Object>()
+		parameters.put("companyName", "PT. Bank Mandiri")
+		JasperPrint print = JasperFillManager.fillReport(report, parameters)
+		JasperExportManager.exportReportToPdfFile(print, "./Data")
+		
+	}
+}
+
